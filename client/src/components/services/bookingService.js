86f22389e938ed805e79b6671fb972842989d10f
poojithavarli07 <poojithavarli@gmail.com>
2026-07-21 21:07:39 +0530

    Delete client directory

--- client/src/components/services/bookingService.js
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export const createBooking = async (bookingData) => {
-  const response = await api.post("/bookings", bookingData);
-  return response.data;
-};
--- client/src/components/services/nurseService.js
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export const getNurses = async () => {
-  const response = await api.get("/nurses");
-  return response.data;
-};
